46d8df99e466353354e7a5cf05a62224b1a3cedc
wlu <573253193@qq.com>
2024-10-22 13:21:06 +0800

    exr 改norm

--- dolphins_bench_attack_exr.py
+++ dolphins_bench_attack_exr.py
@@ -167,13 +167,26 @@ def get_model_inputs_prompts(instruction, model, tokenizer):
     # print(prompt)
     return inputs
 
+def normalize(tensor, mean, std):
+    mean = torch.tensor(mean).view(1, 3, 1, 1).half().to(tensor.device)
+    std = torch.tensor(std).view(1, 3, 1, 1).half().to(tensor.device)
+    return (tensor - mean) / std
+
+def denormalize(tensor, mean, std):
+    mean = torch.tensor(mean).view(1, 3, 1, 1).half().to(tensor.device)
+    std = torch.tensor(std).view(1, 3, 1, 1).half().to(tensor.device)
+    return tensor * std + mean
+
 def exr_attack(model, vision_x, input_ids_list, attention_mask_list, labels_list=None, epsilon=0.001, steps=10, lp='linf', dire='pos'):
     noise = torch.zeros_like(vision_x).to(device).half().cuda()
     alpha = epsilon / steps
+    denormed_vision_x = denormalize(vision_x, image_mean, image_std)
     for idx in range(steps):
         noise.requires_grad = True
+        vision_x_noise = denormed_vision_x.half().cuda() + noise
+        vision_x_noise = normalize(vision_x_noise, image_mean, image_std)
         loss = model(
-            vision_x=vision_x.half().cuda() + noise,
+            vision_x=vision_x_noise,
             lang_x=input_ids_list[idx].cuda(),
             attention_mask=attention_mask_list[idx].cuda(),
             labels=labels_list[idx].cuda(),
@@ -196,6 +209,8 @@ def exr_attack(model, vision_x, input_ids_list, attention_mask_list, labels_list
         noise = noise.detach()
     return noise.detach()
 
+image_mean = [0.48145466, 0.4578275, 0.40821073]
+image_std = [0.26862954, 0.26130258, 0.27577711]
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
